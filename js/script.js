78847b1b5491c1df966cd1a94516596134925b7f
Toby King <tobyking13@gmail.com>
2023-02-17 13:25:05 +0000

    updates title

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-const blob = document.getElementById("blob");
-
 document.body.onpointermove = e => {
+    const blob = document.getElementById("blob");
     const {clientX, clientY} = e;
 
     console.log(e)
@@ -11,3 +10,24 @@ document.body.onpointermove = e => {
     }, {duration: 2000, fill: "forwards"});
     
 }
+
+document.getElementById("title").onmouseover = e => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let interations = 0;
+    const interval = setInterval(() => {
+    e.target.innerText = e.target.innerText.split('')
+    .map((letter, index) => {
+        if (index < interations) {
+           return e.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join('');
+    if (interations >= title.length) {
+        clearInterval(interval)
+    }
+    console.log(interations)
+    interations += 1/3;
+}, 30)
+}
+    
