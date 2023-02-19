bdafde1025194267e5bfbbebd3ff81a9dec22366
Toby King <tobyking13@gmail.com>
2023-02-19 11:46:20 +0000

    updates portfolio, adds some text

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 document.body.onpointermove = e => {
     const blob = document.getElementById("blob");
     const {clientX, clientY} = e;
-
-    console.log(e)
-
     blob.animate({
         left: `${clientX}px`,
         top: `${clientY}px`
     }, {duration: 2000, fill: "forwards"});
-    
 }
 
 document.getElementById("title").onmouseover = e => {
@@ -26,8 +22,20 @@ document.getElementById("title").onmouseover = e => {
     if (interations >= title.length) {
         clearInterval(interval)
     }
-    console.log(interations)
     interations += 1/3;
 }, 30)
 }
     
+for(const link of document.getElementsByClassName("link")) {
+    link.onmousemove = e => {
+        const decimal = e.clientX / link.offsetWidth;
+
+        const   basePercent = 80,
+                percentRange = 20,
+                adjustablePercent = percentRange * decimal;
+
+        const lightPercent = basePercent + adjustablePercent;
+
+        link.style.setProperty("--light-percent", `${lightPercent}%`)
+    }
+}
